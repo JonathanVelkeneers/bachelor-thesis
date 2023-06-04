fb2fc9f177966c7f5b7d56044758c889425edf97
Jonathan Velkeneers <jonathan.velkeneers@ida-mediafoundry.be>
2023-06-04 19:57:22 +0200

    Init commit

---
+++ MauiPokedex/MauiPokedex/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiPokedex;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiPokedex/MauiPokedex/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiPokedex;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
